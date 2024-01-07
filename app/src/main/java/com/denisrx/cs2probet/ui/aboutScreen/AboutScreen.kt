7300744aa0e731239a06730a76071edddd26f9b2
DenisRx <denis.roux@epitech.eu>
2024-01-07 10:29:36 +0100

    Create about screen

---
+++ app/src/main/java/com/denisrx/cs2probet/ui/aboutScreen/AboutScreen.kt
@@ -0,0 +1,61 @@
+package com.denisrx.cs2probet.ui.aboutScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.denisrx.cs2probet.R
+
+@Composable
+fun AboutScreen(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.app_padding)),
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.secondary,
+        )
+
+        Image(
+            painter = painterResource(R.drawable.logo_full),
+            contentDescription = stringResource(R.string.application_logo_description),
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.large_padding))
+                .size(dimensionResource(R.dimen.logo_size_about_screen))
+        )
+
+        LazyColumn {
+            item {
+                Text(
+                    text = stringResource(R.string.app_instructions),
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            }
+        }
+    }
+}
